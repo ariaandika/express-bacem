8efa06cf300bf00f5cdd3105d517e5b6583f0629
ariaandika <andikaakun17@gmail.com>
2024-03-03 11:34:34 +0700

    feat: handle cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import parser from "body-parser"
 import { logging } from "./middleware/logging.js"
+import cors from "cors"
 import { users } from "./model/users.js"
 import { products } from "./model/products.js"
 
@@ -8,8 +9,9 @@ const app = express()
 
 app.use(parser.json())
 app.use(logging)
+app.use(cors())
 app.use("/users",users)
 app.use("/products",products)
 
-app.listen(3000)
+app.listen(3000, () => console.log(`Listening http://localhost:3000`))
 
